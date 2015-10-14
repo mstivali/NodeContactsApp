25ad85442fc2fe7716ea41de1fbc37ce4e4e2ac6
mstivali <matthewstivali@gmail.com>
2015-10-14 16:16:28 -0400

    Finished contacts application

--- public/app/main.js
+++ public/app/main.js
@@ -15,8 +15,40 @@
 			$http.post('/contactlist', $scope.contact).success(function(response) {
 				console.log(response);
 				refresh();
+				clearContact();
 			});
+		};
+
+		$scope.remove = function(id) {
+			console.log(id);
+			$http.delete('/contactlist/' + id).success(function(response) {
+				refresh();
+			});
+		};
+
+		$scope.edit = function(id) {
+			console.log(id);
+			$http.get('/contactlist/' + id).success(function(response) {
+				$scope.contact = response;
+			});
+		};
+
+		$scope.update = function() {
+			console.log($scope.contact._id);
+			$http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(){
+				refresh();
+				clearContact();
+			});
+		};
+
+		$scope.deselect = function() {
+			clearContact();
 		}
+
+		function clearContact() {
+			$scope.contact = {};
+		}
+
 
 		function refresh() {
 			$http.get('/contactlist').success(function(response) {
@@ -24,6 +56,8 @@
 				$scope.contactList = response;
 			});
 		}
+
+
 	}
 
 })();
